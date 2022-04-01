37e3e632d59f4d2d4104a7cedb6ca677681fdc40
ForHemer <foreste.hemerson@gmail.com>
2022-04-01 00:17:43 -0400

    Correct error when posting and refreshing data to the API according to the suggestion of the reviewer

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,8 @@
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
-const GET_BOOK = 'bookStore/books/GET_BOOK';
-const url = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/LkccQjVGSRTJTe3ajnr5/books';
+const LIST_BOOK = 'bookStore/books/LIST_BOOK';
+const URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/LkccQjVGSRTJTe3ajnr5/books';
+
 const initialState = [];
 
 const booksReducer = (state = initialState, action) => {
@@ -10,10 +11,10 @@ const booksReducer = (state = initialState, action) => {
       return [...state, action.payload];
 
     case REMOVE_BOOK:
-      return state.filter((book) => book.id !== action.id);
+      return state.filter((book) => book.item_id !== action.id);
 
-    case GET_BOOK:
-      return [...state, action.payload];
+    case LIST_BOOK:
+      return action.payload;
 
     default:
       return state;
@@ -30,16 +31,19 @@ export const removeBook = (id) => ({
   id,
 });
 
+export const listBooks = (payload) => ({
+  type: LIST_BOOK,
+  payload,
+});
+
 export const postBook = (payload) => (
   async (dispatch) => {
-    await fetch(url, {
+    await fetch(URL, {
       method: 'POST',
       body: JSON.stringify({
         item_id: payload.id,
-        title: {
-          bookTitle: payload.title,
-          bookAuthor: payload.author,
-        },
+        title: payload.title,
+        author: payload.author,
         category: payload.category,
       }),
       headers: {
@@ -50,40 +54,52 @@ export const postBook = (payload) => (
   }
 );
 
-export const deleteBook = (id) => (
-  async (dispatch) => {
-    const response = await fetch(`${url}/${id}`, {
-      method: 'DELETE',
-      body: JSON.stringify({
-        item_id: id,
-      }),
-      headers: {
-        'Content-type': 'application/JSON',
-      },
-    });
-    if (response.status === 201) {
-      dispatch(removeBook(id));
-    }
-  }
-);
-
-export const getBook = async (dispatch) => {
-  const response = await fetch(url);
-  const data = await response.json();
-  const books = Object.entries(data);
-  books.forEach((item) => {
-    const [id, value] = item;
-    const { title, category } = value[0];
-    const { bookTitle, bookAuthor } = title;
-
-    const newBook = {
-      id,
-      title: bookTitle,
-      author: bookAuthor,
-      category,
-    };
-    dispatch(addBook(newBook));
+/* export const deleteBook = (id) => async (dispatch) => {
+  const DELETE_BOOK = `${URL}/${id}`;
+  const response = await fetch(DELETE_BOOK, {
+    method: 'DELETE',
+    headers: {
+      'Content-type': 'application/JSON',
+    },
   });
+
+  if (response.status === 201) {
+    dispatch(removeBook(id));
+  }
+}; */
+
+/* export const getBooks = async (dispatch) => {
+  const response = await fetch(URL);
+  const data = await response.json();
+  const books = Object.getOwnPropertyNames(data).map((id) => ({
+    item_id: id,
+    ...data[id][0],
+  }));
+  dispatch(listBooks(books));
+}; */
+
+export const deleteBook = (id) => async (dispatch) => {
+  const DELETE_BOOK = `${URL}/${id}`;
+  const response = await fetch(DELETE_BOOK, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  if (response.ok) {
+    dispatch(removeBook(id));
+  }
+};
+
+export const getBooks = () => async (dispatch) => {
+  const response = await fetch(URL);
+  const APIbooks = await response.json();
+  const books = Object.getOwnPropertyNames(APIbooks).map((id) => ({
+    item_id: id,
+    ...APIbooks[id][0],
+  }));
+  dispatch(listBooks(books));
 };
 
 export default booksReducer;
